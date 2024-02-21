45efb113056add056ac906251bae576c4b5f2ba5
adhinugroho623@gmail.com <adhinugroho623@gmail.com>
2024-02-21 07:51:50 +0700

    add-episode

--- data_video.js
+++ data_video.js
@@ -10,10 +10,19 @@
                 "gendre2": "Historicial",
                 "gendre3": "Drama",
                 "description": "menceritakan tentang petualangan Musa. Ia adalah Seorang remaja yang bekerja sebagai tukang semir sepatu yang menjadi kurir bagi perjuangan arek-arek Suroboyo pada pertempuran 10 November 1945.<br><br> Cerita diawali dengan visualisasi dahsyat dari pengeboman kota Hiroshima oleh Sekutu yang menandakan menyerahnya Jepang. <br><br>“Indonesia merdeka, itu yang kudengar di RRI, Jepang menyerah!”, kata Musa. Tetapi langit Surabaya kembali merah dengan peristiwa Insiden Bendera dan kedatangan Sekutu yang ditumpangi oleh Belanda.",
-                "bgimg": "https://upload.wikimedia.org/wikipedia/id/thumb/a/af/Battle_of_Surabaya.jpeg/444px-Battle_of_Surabaya.jpeg"
+                "bgimg": "https://upload.wikimedia.org/wikipedia/id/thumb/a/af/Battle_of_Surabaya.jpeg/444px-Battle_of_Surabaya.jpeg",
+"episodes": [
+        {
+       "episodeTitle": "Episode 1", 
+       "episodeUrl": "https://pixeldrain.com/api/file/jLXe89mP",
 
-                
+                    
             },
+       
+    
+        ]
+                
+ },
             "boboiboythemovie": {
                 "title": "Boboiboy The Movie",
             "terjemah": "Boboiboy The Movie",
@@ -24,8 +33,15 @@
                 "gendre3": "Adventure",
                 "rilis": "3 Maret 2016",
                 "description": "Ochobot, si robot teman BoBoiBoy, telah diculik oleh sebuah komplotan pemburu harta karun asing berjulukan The Tengkotak, dan akan mereka gunakan untuk melacak lokasi Sfera Kuasa kuno yang bersembunyi di Bumi.",
-                "bgimg": "https://upload.wikimedia.org/wikipedia/id/2/2f/Boboiboy_The_Movies.jpg"
-                
+                "bgimg": "https://upload.wikimedia.org/wikipedia/id/2/2f/Boboiboy_The_Movies.jpg",
+"episodes": [
+        {
+       "episodeTitle": "Episode 1", 
+       "episodeUrl": "https://acefile.co/player/51182839",
+
+                    
+       }
+        ]
             },
 
             "upinipinkerissiamangtunggal": {
@@ -38,7 +54,15 @@
                 "studio": "Les' Copaque Production",
                 "rilis": "9 Mei 2019",
                 "description": "Upin & Ipin: Keris Siamang Tunggal adalah film petualangan dan fantasi terbaru dari Upin & Ipin dan keenam sahabatnya: Ehsan, Fizi, Mail, Jarjit, Mei Mei, dan Susanti dalam menyelamatkan kerajaan fantasi Inderaloka dari ancaman musuhnya, Raja Bersiong yang jahat. Cerita bermula saat Upin, Ipin, dan para sahabatnya tak sengaja menemukan keris mistis di gudang Tok Dalang yang ternyata membuka portal menuju kerajaan Inderaloka.",
-                "bgimg": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8OHumwVLkKLp5EI05xP20NbZJKDmhFAiUsQ&usqp=CAU"
+                "bgimg": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8OHumwVLkKLp5EI05xP20NbZJKDmhFAiUsQ&usqp=CAU",
+"episodes": [
+        {
+       "episodeTitle": "Episode 1", 
+       "episodeUrl": "https://www.youtube.com/embed/6tmIF7RlMA4?feature=shared",
+
+                    
+       }
+        ]
                 
             },
             "onepiecemv14": {
@@ -51,8 +75,15 @@
                 "gendre2": "Adventure",
                 "gendre3": "Fantasy",
                 "description": "Eksposisi perompak terhebat di dunia, oleh para perompak, untuk para perompak – Festival Perompak. Luffy dan kru Topi Jerami lainnya menerima undangan dari tuan rumahnya Buena Festa yang dikenal sebagai Master of Festivities. Mereka tiba untuk menemukan tempat yang penuh dengan paviliun glamor dan banyak perompak termasuk yang dari Generasi Terburuk. Tempatnya listrik.",
-                "bgimg": "https://nimegami.id/wp-content/uploads/2020/02/images-2020-02-22T112315.305.jpeg"
+                "bgimg": "https://nimegami.id/wp-content/uploads/2020/02/images-2020-02-22T112315.305.jpeg",
+"episodes": [
+        {
+       "episodeTitle": "Episode 1", 
+       "episodeUrl": "https://acefile.co/player/19320220",
 
+                    
+       }
+        ]
                 
             },
 "mashleseason2": {
@@ -65,8 +96,15 @@
                 "gendre2": "Comedy",
                 "gendre3": "Fantasy",
                 "description": "menceritakan seorang anak bernama Mash Burnedead yang hidup di dunia yang penuh dengan sihir.<br><br>Dalam dunia sihir tersebut, seorang yang tidak memiliki sihir akan dianggap sebagai aib dan harus diasingkan.<br><br>Tokoh utama Mash Burnedead adalah salah satu manusia yang hidup dengan tidak memiliki sihir apapun.<br><br>Dibuang oleh keluarganya dan ditemukan seorang kakek, kemudia diadopsi dan hidup ditengah hutan.",
-                "bgimg": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI2wCYBc9vA_7Ozookt3gIpnOqclHMAXrLI6GO4AaE6Pq-RIsX2y3j7NKR&s=10"
+                "bgimg": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI2wCYBc9vA_7Ozookt3gIpnOqclHMAXrLI6GO4AaE6Pq-RIsX2y3j7NKR&s=10",
+"episodes": [
+        {
+       "episodeTitle": "Episode 1", 
+       "episodeUrl": "https://acefile.co/player/102743749",
 
+                    
+       }
+        ]
                 
             },
 
@@ -80,8 +118,15 @@
                 "gendre2": "Drama",
                 "gendre3": "Animasi",
                 "description": "Setelah anaknya tertangkap jaring nelayan di Great Barrier Reef dan dibawa ke Sydney, seekor clownfish pemalu bertekad menempuh perjalanan panjang penuh bahaya untuk menemukannya kembali.",
-                "bgimg": "https://static.republika.co.id/uploads/member/images/news/pu2ugznury.jpg"
+                "bgimg": "https://static.republika.co.id/uploads/member/images/news/pu2ugznury.jpg",
+"episodes": [
+        {
+       "episodeTitle": "Episode 1", 
+       "episodeUrl": "https://pixeldrain.com/api/file/uiTWGWbK",
 
+                    
+       }
+        ]
                 
             },
 
@@ -132,17 +177,38 @@
             const terjemahElement = document.getElementById("terjemah");
             const rilisElement = document.getElementById("rilis");
             const ratingElement = document.getElementById("rating");
+const studioElement = document.getElementById("studio");
+const imgElement = document.getElementById("bgimg");
+
+const descriptionElement = document.getElementById("description");
+const episodeContainer = document.getElementById("episodeContainer");
 const gendre1Element = document.getElementById("g1");
 const gendre2Element = document.getElementById("g2");
 const gendre3Element = document.getElementById("g3");
 
 
+// Mendapatkan parameter dari URL untuk menentukan animasi yang dipilih
+// Mengatur konten detail animasi berdasarkan animasi yang dipilih
 
-            const studioElement = document.getElementById("studio");
-            const imgElement = document.getElementById("bgimg");
+    
+
+    // Mengambil data episode dari objek data
+    const episodes = data[animation].episodes;
+
+    // Membuat tombol episode untuk setiap episode
+    episodes.forEach((episode) => {
+        const episodeButton = document.createElement("button");
+        episodeButton.textContent = episode.episodeTitle;
+        episodeButton.addEventListener("click", function() {
+            window.location.href = episode.episodeUrl;
+        });
+        episodeContainer.appendChild(episodeButton);
+    });
+
+
             
-            const descriptionElement = document.getElementById("description");
-            const watchButton = document.getElementById("watchButton");
+           // const watchButton = document.getElementById("watchButton");
+
 
             titleElement.textContent = data[animation].title;
             imgElement.src = data[animation].bgimg;
@@ -154,9 +220,7 @@ const gendre3Element = document.getElementById("g3");
 gendre1Element.innerHTML = data[animation].gendre1;
 gendre2Element.innerHTML = data[animation].gendre2;
 gendre3Element.innerHTML = data[animation].gendre3;
-            watchButton.addEventListener("click", function() {
-                window.location.href = "video_testing.html?animation=" + animation;
-            });
-        }
+            
+        };
         
         

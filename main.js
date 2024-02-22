ab9a2c40586eff43f2c785c5d69dd0a290a2689a
adhinugroho623@gmail.com <adhinugroho623@gmail.com>
2024-02-22 19:13:46 +0700

    data-search-locals

--- main.js
+++ main.js
@@ -24,3 +24,23 @@ function goToWebsite(websiteURL) {
       window.location.href = websiteURL;
       //alert("Oops! Terjadi kesalahan. 4000");
     }
+    
+    
+    const searchInput = document.getElementById("searchInput");
+const dataItems = document.querySelectorAll(".data-item");
+
+searchInput.addEventListener("input", function() {
+  const searchQuery = searchInput.value.toLowerCase();
+  
+  dataItems.forEach(item => {
+    const itemText = item.textContent.toLowerCase();
+    
+    if (itemText.includes(searchQuery)) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+      
+    }
+  });
+});
+

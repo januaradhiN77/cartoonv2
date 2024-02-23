bedcc9c828f20e705b5be543fbf40dc3f0703639
adhinugroho623@gmail.com <adhinugroho623@gmail.com>
2024-02-23 08:45:09 +0700

    konfigurasi

--- data_play_video.js
+++ data_play_video.js
@@ -3,33 +3,33 @@ const data = {
                 "title": "Battle Of Surabaya",
 "episode": "Episode 1",
                 "description": "menceritakan tentang petualangan Musa. Ia adalah Seorang remaja yang bekerja sebagai tukang semir sepatu yang menjadi kurir bagi perjuangan arek-arek Suroboyo pada pertempuran 10 November 1945.<br><br> Cerita diawali dengan visualisasi dahsyat dari pengeboman kota Hiroshima oleh Sekutu yang menandakan menyerahnya Jepang. <br><br>“Indonesia merdeka, itu yang kudengar di RRI, Jepang menyerah!”, kata Musa. Tetapi langit Surabaya kembali merah dengan peristiwa Insiden Bendera dan kedatangan Sekutu yang ditumpangi oleh Belanda.",
-                "videoUrl": "https://pixeldrain.com/api/file/jLXe89mP"
+                
             },
             
             "boboiboythemovie": {
                 "title": "Boboiboy The Movie",
 "episode": "Episode 1",
                 "description": "Ochobot, si robot teman BoBoiBoy, telah diculik oleh sebuah komplotan pemburu harta karun asing berjulukan The Tengkotak, dan akan mereka gunakan untuk melacak lokasi Sfera Kuasa kuno yang bersembunyi di Bumi.",
-                "videoUrl": "https://acefile.co/player/51182839"
+                
             },
             "upinipinkerissiamangtunggal": {
                 "title": "Upin & Ipin Keris Siamang Tunggal",
 "episode": "Episode 1",
                 "description": "Upin & Ipin: Keris Siamang Tunggal adalah film petualangan dan fantasi terbaru dari Upin & Ipin dan keenam sahabatnya: Ehsan, Fizi, Mail, Jarjit, Mei Mei, dan Susanti dalam menyelamatkan kerajaan fantasi Inderaloka dari ancaman musuhnya, Raja Bersiong yang jahat. Cerita bermula saat Upin, Ipin, dan para sahabatnya tak sengaja menemukan keris mistis di gudang Tok Dalang yang ternyata membuka portal menuju kerajaan Inderaloka.",
-                "videoUrl": "https://www.youtube.com/embed/6tmIF7RlMA4?feature=shared"
+                
             },
 "onepiecemv14": {
 "title": "One Piece Movie 14 : Stampede",
 "episode": "Episode 1",
 "description": "Eksposisi perompak terhebat di dunia, oleh para perompak, untuk para perompak – Festival Perompak. Luffy dan kru Topi Jerami lainnya menerima undangan dari tuan rumahnya Buena Festa yang dikenal sebagai Master of Festivities. Mereka tiba untuk menemukan tempat yang penuh dengan paviliun glamor dan banyak perompak termasuk yang dari Generasi Terburuk. Tempatnya listrik.",
-"videoUrl": "https://acefile.co/player/19320220"
+
             },
 
 "mashleseason2": {
 "title": "Mashle Season 2",
 "episode": "Episode 1",
 "description": "menceritakan seorang anak bernama Mash Burnedead yang hidup di dunia yang penuh dengan sihir.<br><br>Dalam dunia sihir tersebut, seorang yang tidak memiliki sihir akan dianggap sebagai aib dan harus diasingkan.<br><br>Tokoh utama Mash Burnedead adalah salah satu manusia yang hidup dengan tidak memiliki sihir apapun.<br><br>Dibuang oleh keluarganya dan ditemukan seorang kakek, kemudia diadopsi dan hidup ditengah hutan.",
-"videoUrl": "https://acefile.co/player/102743749"
+
             },
 
 
@@ -38,7 +38,7 @@ const data = {
 "title": "Finding Nemo",
 "episode": "Episode 1",
 "description": "Setelah anaknya tertangkap jaring nelayan di Great Barrier Reef dan dibawa ke Sydney, seekor clownfish pemalu bertekad menempuh perjalanan panjang penuh bahaya untuk menemukannya kembali.",
-"videoUrl": ""
+
             },
 
 
@@ -70,25 +70,13 @@ const data = {
 
         // Mengatur video yang akan ditampilkan berdasarkan animasi yang dipilih
         if (animation && data.hasOwnProperty(animation)) {
-            const videoPlayer = document.getElementById("videoPlayer");
-            const videoSource = document.getElementById("videoSource");
             const titleElement = document.getElementById("title");
             const descriptionElement = document.getElementById("description");
-            const videoContainer = document.getElementById("videoContainer");
+            
               const episodeElement = document.getElementById("episode");
 
 titleElement.innerHTML = data[animation].title;
             episodeElement.textContent = data[animation].episode;
             descriptionElement.innerHTML = data[animation].description;
-const iframe = document.createElement("iframe");
-            iframe.src = data[animation].videoUrl;
-            iframe.width = "100%";
-            iframe.height = "220";
-            iframe.frameBorder = "0";
-            iframe.allowFullscreen = true;
 
-            videoContainer.appendChild(iframe);
-           // videoSource.src = data[animation].videoUrl;
-            //videoPlayer.load();
-            //videoPlayer.play();
         }
